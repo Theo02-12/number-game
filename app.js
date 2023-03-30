0f4eb1c1fb8f60e87669dcddc64ee61158a9a6e4
Theo02-12 <mrcouvelard@gmail.com>
2023-03-30 10:36:43 +0200

    foncionne

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
-function startGame(){
+function startGame() {
 
-    const random = (max, min) =>{
+    const random = (max, min) => {
         return Math.floor(Math.random() * (max - min) - min)
     }
 
     var resultRandom = random(11, 0)
     console.log(resultRandom)
 
-const btntestNumber = document.querySelector('#testNumber')
-btntestNumber.addEventListener('click', testNumber)
-let i = 2;
+    const btntestNumber = document.querySelector('#testNumber')
+    btntestNumber.addEventListener('click', testNumber)
+    let i = 2;
 
-var textWin = document.createElement("h1")
-textWin.textContent = "bravo"
-var text = document.createElement("h1")
-text.textContent = "dommage"
+    var textWin = document.createElement("h1")
+    var br = document.createElement("br")
+    container.append(textWin)
 
-    function testNumber(){
-    
-    console.log(i)
-    
-    const selectNumber = document.querySelector('#chiffre').value;
-    const container = document.querySelector('#container')
-    
-    
-    if(selectNumber == resultRandom){
-        container.append(textWin)
-        
-    }else{
-        container.appendChild(text)
-        
+    function testNumber() {
+
+        console.log(i)
+
+        const selectNumber = document.querySelector('#chiffre').value;
+        const container = document.querySelector('#container')
+
+
+        if (selectNumber == resultRandom) {
+            textWin.innerHTML = "bravo c'est gagné";
+            playAgain.style.display = "block";
+
+        } else {
+            textWin.innerHTML += `dommage ${i} essais restant`
+            textWin.append(br)
+
+        }
+
+
+
+        if (i > 0) {
+            i--;
+        } else if (i == 0) {
+            playAgain.style.display = "block";
+        }
     }
-  
-    if(i > 0){
-        i--;
-    }else if (i == 0){
-      alert('0 essais restant')
+
+    const playAgain = document.querySelector('#rejouer')
+    playAgain.addEventListener('click', rejouer);
+    
+    function rejouer() {
+        location.reload();
     }
-}
-
-
 }
 
 
